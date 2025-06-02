4188d812bcd0c458f887f02fd04419dbd4f75e4a
Vladyslav Savisko <savskovladik@gmail.com>
2025-06-03 00:10:36 +0300

    debuging

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -3,3 +3,11 @@ declare module "*.module.css" {
   const classes: { [key: string]: string };
   export default classes;
 }
+
+interface ImportMetaEnv {
+  readonly VITE_TMDB_TOKEN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
